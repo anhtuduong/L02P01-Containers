b6a1d004a0c06c56abb2b1451201a4b4e16b96e4
Anh Tu Duong <anhtu.duong@studenti.unitn.it>
2024-05-05 16:22:37 +0200

    clean.bat

--- clean.bat
+++ clean.bat
@@ -1,14 +1,14 @@
 @REM # Shut off all the containers
-
+call docker compose down
 
 @REM # Remove the spring application image
-
+call docker rmi eist-ngrok:latest
 @REM # Remove the postgres database image
-
+call docker rmi postgres:latest
 @REM # Remove the ngrok image
-
+call docker rmi ngrok/ngrok:latest
 
 @REM # Remove the `/build` directory
-
+rmdir /s /q build
 @REM # Remove the `/webapp` directory
-
+rmdir /s /q webapp
