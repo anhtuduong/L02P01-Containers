ba6522e906a98eacd8d89f27d02a824ccc1f6d11
Anh Tu Duong <anhtu.duong@studenti.unitn.it>
2024-05-07 20:07:18 +0200

    attempt integration

--- src/test/java/de/tum/in/ase/eist/PersonIntegrationTest.java
+++ src/test/java/de/tum/in/ase/eist/PersonIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -73,5 +74,132 @@ class PersonIntegrationTest {
         assertTrue(personRepository.findAll().isEmpty());
     }
 
-    // TODO: Add more test cases here
+    @Test
+    void testAddParent() throws Exception {
+        // Create parent
+        var parent = createAndSavePerson("Reino", "Wendell");
+        // Create child
+        var child = createAndSavePerson("Wilfreda", "Steube");
+
+        // Check if all Person is saved to database
+        assertEquals(2, personRepository.findAll().size());
+
+        // Add parent to child
+        var response = this.mvc.perform(
+                put("/persons/{childId}/parents", child.getId())
+                        .content(objectMapper.writeValueAsString(parent))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+        // Check if parent-child relationship is established
+//        assertEquals(1, child.getParents().size());
+//        assertTrue(child.getParents().contains(parent));
+    }
+
+    @Test
+    void testAddThreeParents() throws Exception {
+        // Create parent1
+        var parent1 = new Person();
+        parent1.setFirstName("Ben");
+        parent1.setLastName("Feuchtwanger");
+        parent1.setBirthday(LocalDate.now());
+
+        var response = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(parent1))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        // Check if this person is saved to database
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(1, personRepository.findAll().size());
+
+        // Create parent2
+        var parent2 = new Person();
+        parent2.setFirstName("Arne");
+        parent2.setLastName("Hohenstein");
+        parent2.setBirthday(LocalDate.now());
+
+        response = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(parent2))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        // Check if this person is saved to database
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(2, personRepository.findAll().size());
+
+        // Create parent3
+        var parent3 = new Person();
+        parent3.setFirstName("Silvio");
+        parent3.setLastName("Hauer");
+        parent3.setBirthday(LocalDate.now());
+
+        response = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(parent3))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        // Check if this person is saved to database
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(3, personRepository.findAll().size());
+
+        // Create child
+        var child = new Person();
+        child.setFirstName("Fynn");
+        child.setLastName("Hirsch");
+        child.setBirthday(LocalDate.now());
+
+        response = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(child))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        // Check if this person is saved to database
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(4, personRepository.findAll().size());
+
+        // Add parent1 to the child
+        var childId = child.getId();
+        response = this.mvc.perform(
+                put("/persons" + "/" + childId + "parents")
+                        .content(objectMapper.writeValueAsString(parent1))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+        // Add parent2 to the child
+        response = this.mvc.perform(
+                put("/persons" + "/" + childId + "parents")
+                        .content(objectMapper.writeValueAsString(parent2))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+        // Check if parent-child relationship is established
+        assertEquals(2, child.getParents().size());
+        assertTrue(child.getParents().contains(parent1));
+        assertTrue(child.getParents().contains(parent2));
+
+        // Try to add the third parent, which should throw an exception
+        response = this.mvc.perform(
+                put("/persons" + "/" + childId + "parents")
+                        .content(objectMapper.writeValueAsString(parent3))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    private Person createAndSavePerson(String firstName, String lastName) {
+        var person = new Person();
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        person.setBirthday(LocalDate.now());
+        return personRepository.save(person);
+    }
 }
